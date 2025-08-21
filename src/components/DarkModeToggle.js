e784c74e7cb32b42f540c9a72c28272e95aecc1e
Jay <jordanbell321@gmail.com>
2025-08-21 16:26:23 +0100

    refactor: Update homepage URL and reorganize styles; remove unused components and styles

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTheme } from "../contexts/ThemeContext";
-import "./DarkModeToggle.css";
+import "../styles/DarkModeToggle.css";
 
 const DarkModeToggle = ({ className = "" }) => {
     const { isDarkMode, toggleTheme } = useTheme();
